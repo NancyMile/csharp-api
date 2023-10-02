f896ef5b0dae4c53156c9e29498b775522df409e
nancy <smithnancy@outlook.com>
2023-10-02 14:53:19 +1100

    BooksController methods functionality added

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -20,34 +20,59 @@ public class BooksController : Controller
     [HttpGet]
     public async Task<ActionResult<IEnumerable<Book>>> GetBooks()
     {
-
+        return await _context.Books.ToListAsync();
     }
 
     //GET api/books/id
     [HttpGet("{id}")]
     public async Task<ActionResult<Book>> GetBook(int id)
     {
+        var book = await _context.Books.FindAsync(id);
 
+        if(book == null)
+            return NotFound();
+
+        return book;
     }
 
     //POST api/books
     [HttpPost]
     public async Task<ActionResult<Book>>  PostBook(Book book)
     {
-
+        _context.Books.Add(book);
+        await _context.SaveChangesAsync();
+        return CreatedAction("GetBook", new {id = book.Id},book);
     }
 
     //PUT api/books/id
     [HttpPut("{id}")]
     public sync Task<IActionResult> PutBook(int id, Book book)
     {
+        if(id != book.Id)
+            return BadRequest();
 
+        var bookInDb = await _context.Books.FindAsync(id);
+
+        if(bookInDb == null)
+            return NotFound();
+
+        _context.Entry(book).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+        return NoContent();
     }
 
     //DELETE api/books/id
     [HttpDelete("{id}")]
     public async Task<ActionResult<Book>> DeleteBook(int id)
     {
+        var book = await _context.Books.FindAsync(id);
 
+        if(book == null)
+            return NotFound();
+
+        _context.Books.Remove(book);
+        await _context.SaveChangesAsync();
+
+        return book;
     }
 }
